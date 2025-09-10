fc72f8f4b707a4baeaf3bb8fba3e08b6ca4d461d
PYTHON_LEARN <jayaprakash25.it@gmail.com>
2025-09-10 21:06:29 +0530

    GOOD

--- if condition basic.py
+++ if condition basic.py
@@ -5,3 +5,23 @@ if x >= 4:
 else:
     print("YOU ARE UNDER AGE")
 
+
+# Number status
+num = int(input("Enter a number: "))
+
+if num > 0:
+    print("Positive")
+elif num == 0:
+    print("Zero")
+else:
+    print("Negative")
+
+#check greater and smaller number
+print("check greater and smaller number")
+x=int(input("Enter a number: "))
+y=int(input("Enter a number: "))
+if x>y:
+    print(f"GREATER NUMBER: {x} \nSMALLER NUMBER: {y}" )
+else:
+    print(f"GREATER NUMBER: {y}\n SMALLER NUMBER: {x}")
+
