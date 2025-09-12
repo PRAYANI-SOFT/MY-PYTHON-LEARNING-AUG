1747796ca2311d1295d8ade07121b6e2a78fa231
PYTHON_LEARN <jayaprakash25.it@gmail.com>
2025-09-12 23:29:48 +0530

    GOOD

--- Random MODULE.py
+++ Random MODULE.py
@@ -1,1 +1,13 @@
 import random
+random1=random.randint(1,2 )
+if random1==1:
+    print("HEAD")
+else :
+    print("TAIL")
+
+
+random0_1=random.random( )
+print(random0_1)
+
+randomA_B=random.uniform(1,10 )
+print(randomA_B)
