8152610e9f1282570f15be013c2bbe78cbb0290c
PYTHON_LEARN <jayaprakash25.it@gmail.com>
2025-09-13 22:04:29 +0530

    GOOD

--- PROJECT ROCK PAPER SCISSORS.py
+++ PROJECT ROCK PAPER SCISSORS.py
@@ -27,23 +27,37 @@ def scissors():
 ---.__(___)
 ''')
 print("LET US PLAY ROCK PAPER SCISSORS")
-choices = ["ROCK", "PAPER", "SCISSOR"]
+choices = ["ROCK", "PAPER", "SCISSORS"]
 choose=input("Do you choose ROCK/PAPER /SCISSORS? : ")
+if choose=="ROCK":
+    rock()
+elif choose=="PAPER":
+    paper()
+else :
+    scissors()
 COMPUTER =(random.choice(choices))
+
+
 print(f"COMPUTER CHOICE:{COMPUTER}")
+if COMPUTER=="ROCK":
+    rock()
+elif COMPUTER=="PAPER":
+    paper()
+else :
+    scissors()
 if choose==COMPUTER:
     print("DRAWN")
-    if choose!=COMPUTER and choose=="ROCK" and COMPUTER=="SCISSOR":
-                print("YOU WON")
-        else:
-             print("YOU LOSE")
-if choose!=COMPUTER and choose=="SCISSOR" and COMPUTER=="PAPER":
-    print("YOU WIN")
-else:
+elif choose=="ROCK" and COMPUTER=="SCISSORS":
+     print("YOU WON")
+elif choose=="SCISSORS" and COMPUTER=="ROCK":
     print("YOU LOSE")
-if choose!=COMPUTER and choose=="PAPER" and COMPUTER=="ROCK":
+elif choose=="SCISSORS" and COMPUTER=="PAPER":
     print("YOU WIN")
-else:
+elif choose=="PAPER" and COMPUTER=="SCISSORS":
+    print("YOU LOSE")
+elif choose=="PAPER" and COMPUTER=="ROCK":
+    print("YOU WIN")
+elif choose == "ROCK" and COMPUTER == "PAPER":
     print("YOU LOSE")
 
 
