06dce863e7df231ce66fd07ff756e8dee5cb7e21
PYTHON_LEARN <jayaprakash25.it@gmail.com>
2025-09-13 20:55:46 +0530

    GOOD

---
+++ 32 LIST.py
@@ -0,0 +1,47 @@
+indian_states = [
+    "Andhra Pradesh",
+    "Arunachal Pradesh",
+    "Assam",
+    "Bihar",
+    "Chhattisgarh",
+    "Goa",
+    "Gujarat",
+    "Haryana",
+    "Himachal Pradesh",
+    "Jharkhand",
+    "Karnataka",
+    "Kerala",
+    "Madhya Pradesh",
+    "Maharashtra",
+    "Manipur",
+    "Meghalaya",
+    "Mizoram",
+    "Nagaland",
+    "Odisha",
+    "Punjab",
+    "Rajasthan",
+    "Sikkim",
+    "Tamil Nadu",
+    "Telangana",
+    "Tripura",
+    "Uttar Pradesh",
+    "Uttarakhand",
+    "West Bengal"
+]
+print(indian_states[9])
+print(indian_states[10])
+print(indian_states[11])
+print(indian_states[1])
+
+fruits = ["Strawberries", "Nectarines", "Apples", "Grapes", "Peaches", "Cherries", "Pears"]
+var = fruits[-5]
+print(var)
+
+fruits = ["Strawberries", "Nectarines", "Apples", "Grapes", "Peaches", "Cherries", "Pears"]
+vegetables = ["Spinach", "Kale", "Tomatoes", "Celery", "Potatoes"]
+
+
+dirty_dozen = [vegetables, fruits]
+print(dirty_dozen[1][1])
+print(dirty_dozen[1][2])
+print(dirty_dozen[1][3])
