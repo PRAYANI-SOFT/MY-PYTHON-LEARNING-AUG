79b585b108579fb87a3f078b38de69e5752f3b28
PYTHON_LEARN <jayaprakash25.it@gmail.com>
2025-09-14 07:48:18 +0530

    GOOD

---
+++ 38 LOOP with list.py
@@ -0,0 +1,27 @@
+from enum import pickle_by_enum_name
+
+fruits = ["apple","banana","orange"]
+for fruit in fruits:
+    print(fruit)
+    """print(fruit.upper())
+    print(fruit.lower())
+    print(fruit.capitalize())
+    print(fruit.title()"""
+    print(fruit + "pie")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
