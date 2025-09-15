65a5f95ac06fb82baadd208872748d8858005e82
PYTHON_LEARN <jayaprakash25.it@gmail.com>
2025-09-15 21:29:54 +0530

    GOOD

---
+++ 54 hangman.py
@@ -0,0 +1,18 @@
+import random
+word_list = ["aardvark","baboon","camel"]
+random_word = (random.choice(word_list))
+print(random_word)
+length = len(random_word)
+print(length)
+while length:
+    print(" _ ",end="")
+    length=length-1
+print("")
+guess= input("Enter the correct letters:").lower()
+
+for letter in random_word:
+    if guess == letter:
+        print( letter ,end=" ")
+    else :
+        print(" _ ",end="")
+
